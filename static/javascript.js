dde4ba2c7a37279d8628f3a804db8f0a99969b64
Introspectivegorilla <annabeledmonston@gmail.com>
2025-07-28 14:59:23 +0000

    MOre layout stuff yay

--- static/javascript.js
+++ static/javascript.js
@@ -11,3 +11,24 @@ function to_my_cards() {
 
 }
 
+function create_new() {
+
+    const container = document.getElementsByClassName("box3 r-corner")[0]
+    const new_card = document.createElement('input')
+    const button = document.getElementsByClassName('btn3')[0]
+
+    const new_card_answer = document.createElement('input')
+    new_card_answer.name="response"
+    new_card_answer.className="answer"
+    new_card_answer.type='text'
+
+    new_card.name = "prompt"
+    new_card.className = "question"
+    new_card.type="text"
+
+
+    container.appendChild(new_card)
+    container.appendChild(new_card_answer)
+    button.after(new_card_answer)
+    button.after(new_card)
+}
